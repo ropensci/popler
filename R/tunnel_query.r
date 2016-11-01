7b948cb2e803037531ad242cd1e8a2a1af44b09e
AldoCompagnoni <aldo.compagnoni@gmail.com>
2016-11-01 11:43:38 -0500

    defined and implemented default columns

--- R/tunnel_query.r
+++ R/tunnel_query.r
@@ -47,8 +47,17 @@ tunnel_query = function(...){
     'spt_rep4', 'trt_label'
   )
 
+  default_columns = paste(c(
+    "year","day","month",                      
+    "kingdom","phylum","clss","family","genus","species",                    
+    "structure","datatype",         
+    "spatial_replication_level_1","spatial_replication_level_2","spatial_replication_level_3",
+    "spatial_replication_level_4","proj_metadata_key"
+  ), collapse = ", ")
+  
+  
   search_arg=parse_to_sql_search(...)
-
+  
   # Testing SQL syntax
   conn <- src_postgres(
     dbname="popler_3", host="www.how-imodel-it.com", port=5432, user="lter", password="bigdata")
@@ -63,11 +72,7 @@ tunnel_query = function(...){
   table_all <- tbl(conn, sql(
     paste(
           # Count data
-          "SELECT year, day, month, kingdom, phylum, clss, family, genus, species,",
-          "structure, datatype, count_observation,",
-          "spatial_replication_level_1, spatial_replication_level_2,", 
-          "spatial_replication_level_3, spatial_replication_level_4,",
-          "proj_metadata_key", # lterid, lat, lng, 
+          "SELECT",default_columns,", count_observation",
           "FROM count_table",
           "JOIN taxa_table ON count_table.taxa_count_fkey = taxa_table.taxa_table_key",
           "JOIN site_in_project_table ON taxa_table.site_in_project_taxa_key =",
@@ -80,11 +85,7 @@ tunnel_query = function(...){
           
           "UNION ALL",
           # Biomass data
-          "SELECT year, day, month, kingdom, phylum, clss, family, genus, species,",
-          "structure,  datatype, biomass_observation,",
-          "spatial_replication_level_1, spatial_replication_level_2,", 
-          "spatial_replication_level_3, spatial_replication_level_4,",
-          "proj_metadata_key", #lterid, lat, lng, 
+          "SELECT",default_columns,", biomass_observation",
           "FROM biomass_table",
           "JOIN taxa_table ON biomass_table.taxa_biomass_fkey = taxa_table.taxa_table_key",
           "JOIN site_in_project_table ON taxa_table.site_in_project_taxa_key =",
@@ -97,11 +98,7 @@ tunnel_query = function(...){
           
           "UNION ALL",
           # percent cover data
-          "SELECT year, day, month, kingdom, phylum, clss, family, genus, species,",
-          "structure,  datatype, percent_cover_observation,",
-          "spatial_replication_level_1, spatial_replication_level_2,", 
-          "spatial_replication_level_3, spatial_replication_level_4,",
-          "proj_metadata_key", #lterid, lat, lng, 
+          "SELECT",default_columns,", percent_cover_observation",
           "FROM percent_cover_table",
           "JOIN taxa_table ON percent_cover_table.taxa_percent_cover_fkey = taxa_table.taxa_table_key",
           "JOIN site_in_project_table ON taxa_table.site_in_project_taxa_key =",
@@ -114,11 +111,7 @@ tunnel_query = function(...){
           
           "UNION ALL",
           # individual data
-          "SELECT year, day, month, kingdom, phylum, clss, family, genus, species,",
-          "structure,  datatype, individual_observation,",
-          "spatial_replication_level_1, spatial_replication_level_2,", 
-          "spatial_replication_level_3, spatial_replication_level_4,",
-          "proj_metadata_key", #lterid, lat, lng, 
+          "SELECT",default_columns,", individual_observation",
           "FROM individual_table",
           "JOIN taxa_table ON individual_table.taxa_individual_fkey = taxa_table.taxa_table_key",
           "JOIN site_in_project_table ON taxa_table.site_in_project_taxa_key =",
@@ -131,11 +124,7 @@ tunnel_query = function(...){
           
           "UNION ALL",
           # density data
-          "SELECT year, day, month, kingdom, phylum, clss, family, genus, species,",
-          "structure,  datatype, density_observation,",
-          "spatial_replication_level_1, spatial_replication_level_2,", 
-          "spatial_replication_level_3, spatial_replication_level_4,",
-          "proj_metadata_key", #lterid, lat, lng, 
+          "SELECT",default_columns,", density_observation",
           "FROM density_table",
           "JOIN taxa_table ON density_table.taxa_density_fkey = taxa_table.taxa_table_key",
           "JOIN site_in_project_table ON taxa_table.site_in_project_taxa_key =",
