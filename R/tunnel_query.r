655d0166722c702fd756f04e1c82f024acdecec9
AldoCompagnoni <aldo.compagnoni@gmail.com>
2016-11-02 11:08:48 -0500

    New arguments add_column and subtract_column, to add and subtract columns to the query, and metadata to automatically pull metadata on each study

--- R/tunnel_query.r
+++ R/tunnel_query.r
@@ -2,6 +2,8 @@
 #'
 #' This function browses LTER studies contained in the popler database. The user can group, tally, and subset the data sets based on a number of the factors (or columns) of the database.
 #' @param ... A logical expression using R synthax.
+#' @param add_columns A string that to specify which columns to add in the default columns used in queries to the database. See Details for default columns.
+#' @param subtract_columns A string to specify which columns, among the default columsn, the user wishes to discard in queries to the database 
 #' @return A data frame of the selected data.
 #' @export
 #' @examples
@@ -13,49 +15,23 @@
 
 
 #Function that connects and gathers the information from the database
-tunnel_query = function(...){
+tunnel_query = function(..., add_columns = NULL, subtract_columns = NULL,
+                        metadata = T){
 
-  # Database columns that will be stored as an attribute in the popler Class
-  searchable_columns = c(
-    # site_table
-    'lterid', 'siteid', 'lat', 'long', 'decsript',
-
-    # main_table
-    'proj_metadata_key', 'datatype', 'structured', 
-    'sitestartyr', 'siteendyr', 'samplefreq', 
-    'studytype', 'community',
-
-    # -- Spatial replication extent (numeric value)
-    'spatial_replication_level_1_extent', 'spatial_replication_level_2_extent', 
-    'spatial_replication_level_3_extent', 'spatial_replication_level_4_extent',
-    # -- Spatial replication units (variable character)
-    'spatial_replication_level_1_extent_units, spatial_replication_level_2_extent_units', 
-    'spatial_replication_level_3_extent_units, spatial_replication_level_4_extent_units',
-    # -- Spatial replication original label (variable character)
-    'spatial_replication_level_1_label', 'spatial_replication_level_2_label', 
-    'spatial_replication_level_3_label', 'spatial_replication_level_4_label',
-    # -- Number of unique levels within each group of Spatial replication (integer)
-    'spatial_replication_level_1_number_of_unique_reps', 'spatial_replication_level_2_number_of_unique_reps', 
-    'spatial_replication_level_3_number_of_unique_reps', 'spatial_replication_level_4_number_of_unique_reps',
-    'author', 'knbid',
-
-    # taxa_table
-    'sppcode', 'kingdom', 'phylum', 'clss', 'ordr', 'family', 'genus', 'species',
-
-    # raw_table
-    'year', 'month', 'day', 'covariates', 'spt_rep1', 'spt_rep2', 'spt_rep3',
-    'spt_rep4', 'trt_label'
-  )
-
-  default_columns = paste(c(
+  default_columns = c(
     "year","day","month",                      
-    "kingdom","phylum","clss","family","genus","species",                    
+    "genus","species",                    
     "structure","datatype",         
     "spatial_replication_level_1","spatial_replication_level_2","spatial_replication_level_3",
-    "spatial_replication_level_4","proj_metadata_key"
-  ), collapse = ", ")
+    "spatial_replication_level_4",
+    "proj_metadata_key"
+  )
   
+  # Add or subtract, or define columns
+  default_columns = c(default_columns,add_columns)
+  select_columns = paste( setdiff(default_columns,subtract_columns), collapse = ", ")
   
+  # subset argument
   search_arg=parse_to_sql_search(...)
   
   # Testing SQL syntax
@@ -68,11 +44,12 @@ tunnel_query = function(...){
   # col names
   #col_nam <- as.data.frame(tbl(conn, sql( "SELECT column_name FROM information_schema.columns WHERE
   #                                         table_name='count_table'")))[,1]
+  
 
   table_all <- tbl(conn, sql(
     paste(
           # Count data
-          "SELECT",default_columns,", count_observation",
+          "SELECT",select_columns,", count_observation",
           "FROM count_table",
           "JOIN taxa_table ON count_table.taxa_count_fkey = taxa_table.taxa_table_key",
           "JOIN site_in_project_table ON taxa_table.site_in_project_taxa_key =",
@@ -85,7 +62,7 @@ tunnel_query = function(...){
           
           "UNION ALL",
           # Biomass data
-          "SELECT",default_columns,", biomass_observation",
+          "SELECT",select_columns,", biomass_observation",
           "FROM biomass_table",
           "JOIN taxa_table ON biomass_table.taxa_biomass_fkey = taxa_table.taxa_table_key",
           "JOIN site_in_project_table ON taxa_table.site_in_project_taxa_key =",
@@ -98,7 +75,7 @@ tunnel_query = function(...){
           
           "UNION ALL",
           # percent cover data
-          "SELECT",default_columns,", percent_cover_observation",
+          "SELECT",select_columns,", percent_cover_observation",
           "FROM percent_cover_table",
           "JOIN taxa_table ON percent_cover_table.taxa_percent_cover_fkey = taxa_table.taxa_table_key",
           "JOIN site_in_project_table ON taxa_table.site_in_project_taxa_key =",
@@ -111,7 +88,7 @@ tunnel_query = function(...){
           
           "UNION ALL",
           # individual data
-          "SELECT",default_columns,", individual_observation",
+          "SELECT",select_columns,", individual_observation",
           "FROM individual_table",
           "JOIN taxa_table ON individual_table.taxa_individual_fkey = taxa_table.taxa_table_key",
           "JOIN site_in_project_table ON taxa_table.site_in_project_taxa_key =",
@@ -124,7 +101,7 @@ tunnel_query = function(...){
           
           "UNION ALL",
           # density data
-          "SELECT",default_columns,", density_observation",
+          "SELECT",select_columns,", density_observation",
           "FROM density_table",
           "JOIN taxa_table ON density_table.taxa_density_fkey = taxa_table.taxa_table_key",
           "JOIN site_in_project_table ON taxa_table.site_in_project_taxa_key =",
@@ -133,10 +110,23 @@ tunnel_query = function(...){
           "project_table.proj_metadata_key",
           "JOIN study_site_table ON site_in_project_table.study_site_table_fkey =",
           "study_site_table.study_site_key",
-          "WHERE", search_arg)))
+          "WHERE", search_arg
+    )
+  ))
   
   output_data <- as.data.frame(table_all)
 
+  if(metadata == T) {
+    unique_projects = unique(output_data$proj_metadata_key)
+    for(i in 1:length(unique_projects)){
+      
+      metadata_webpage(unique_projects[i])
+      
+    }
+  }
+  
+  #names(output_data)[grep("count_observation",names(output_data))] <- "abundance"
+  
   return(output_data)
 
 }
