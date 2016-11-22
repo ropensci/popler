5cacba807b3ad5facee3c6cf87340f950fb1b4bc
AldoCompagnoni <aldo.compagnoni@gmail.com>
2016-11-21 21:56:43 -0600

    Service function for get_data. Uses dplyr's translate_sql to trasnlate R logical synthax in sql statements

---
+++ R/parse_to_sql_search.R
@@ -0,0 +1,34 @@
+#' Translate R logical statements into SQL
+#'
+#' USe dplyr translate_sql() to translate R logical statements into SQL
+#' @param ... A logical expression that specifies which fields to keep.
+#' @param lazy should lazy evaluation of logical expression be used?
+#' @return A string with an SQL statement
+#' @export
+
+#This function procudes a string with an sql statement
+#From an R statement that could be used on "
+parse_to_sql_search = function(...,lazy=F){
+  
+  #Use Hadley's translate_sql
+  if(lazy == T){ # with lazy evaluation
+    sqlTranslated=translate_sql(...)
+    
+    # Remove quotation marks
+    sqlPaste=gsub('"',"",sqlTranslated)
+  } else { # without lazy evaluation
+    sqlTranslated=translate_sql_(...)
+    
+    # Remove quotation marks
+    sqlPaste=gsub('"','',sqlTranslated)
+  }
+  
+  #Substitute 'class' with 'clss'
+  sqlPaste=gsub("class","clss",sqlPaste)
+  #Substitute 'order' with 'ordr'
+  sqlPaste=gsub("order","ordr",sqlPaste)
+  
+  #Done!
+  return(sqlPaste)
+  
+}
