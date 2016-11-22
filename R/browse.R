d004e1d8cbd6624e43abd456b1ce67a889f6dc94
AldoCompagnoni <aldo.compagnoni@gmail.com>
2016-11-21 21:51:11 -0600

    Attribute *search_argument* allows to query the database. *criteria* became ... . New argument *view*. named *taxonomy* list elements after project ids (taxa_project_#_...). trim = T returns 25 char, not 50. Changed order of columns: title, proj_metadata_id, lterid.

--- R/browse.R
+++ R/browse.R
@@ -2,10 +2,11 @@
 #'
 #' browse() reports the metadata of LTER studies contained in the popler database. 
 #' The user can subset what data and what columns to visualize.  
-#' @param criteria A logical expression to subset popler's main table
+#' @param ... A logical expression to subset popler's main table
 #' @param full_table Should the function return the standard columns, or the full main table?
-#' @param select_columns A vector of characters that specifies which columns of popler's main table should be selected.
-#' @param trim if TRUE, strings are truncated at the 50th character. Default is TRUE.
+#' @param variables A vector of characters that specifies which columns of popler's main table should be selected.
+#' @param trim If TRUE, strings are truncated at the 50th character. Default is TRUE.
+#' @param view If TRUE, opens up a spreadsheet-style data viewer. If view == "fix" it opens the data frame in a text editor rather than a spreadsheet-style viewer
 #' @return A data frame combining the metadata of each project and the taxonomic units associated with each project.
 #' @export
 #' @examples
@@ -22,38 +23,47 @@
 #' plant_data = browse(kingdom == "Plantae")
 #' 
 #' # Select only the data you need
-#' three_columns = browse(select_columns = c("title","genus","species"))
+#' three_columns = browse(variables = c("title","genus","species"))
 
 
 # The browse popler function
-browse <- function(criteria = NULL, full_table = FALSE, select_columns = NULL, trim = TRUE){
+browse <- function(..., full_table = FALSE, variables = NULL, trim = TRUE, view = FALSE){
 
-  # Load main data table and convert factors to characters
+  # LOAD two object data types
+  # Data table; convert factors to characters
   x           <- popler:::factor_to_character(popler:::main_popler)
-
+  
   # Case insensitive matching ("lower" everything)
-  names(x)    <- tolower(names(x))
+  names(x)    <- tolower( names(x) )
 
-  # Select by criteria 
-  subset_data <- popler:::select_by_criteria(x, substitute(criteria))
+  # Select by subset 
+  subset_data <- popler:::select_by_criteria(x, substitute(...) )
   
   # select data based on 
   subset_data <- popler:::table_select(subset_data, full_table)
   
   # If no column specified, return all columns
-  if( is.null(select_columns) ){
+  if( is.null(variables) ){
     out_cols <- subset_data
   } else{
     # Error message if column names are incorrect
-    popler:::err_full_tab(select_columns,names(x))
+    popler:::err_full_tab( variables,names(x) )
     # If not, select said columns
-    out_cols <- subset_data[,select_columns]
+    out_cols <- subset_data[,variables]
   }
   
-  out      <- popler:::elastic_tab(out_cols, shrink = TRUE, full_table)
-  out_trim <- popler:::trim_display(out, trim)
+  out_form <- popler:::elastic_tab(out_cols, shrink = TRUE, full_table)
+  out_form <- popler:::trim_display(out_form, trim)
   
-  # return output
-  return(out_trim)
-
+  # write output
+  if(view == TRUE) View(out_form) ; if(view == "fix") fix(out_form) 
+  
+  # attribute class "popler"
+  #out      <- structure(list(out_form,search_arg), class = "popler") 
+  out            <- structure(out_form, 
+                              class = c(class(out_form),"popler"),
+                              search_argument = substitute(...) )
+  
+  return(out)
+  
 }
