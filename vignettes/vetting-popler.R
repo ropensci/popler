99ea6c8fa4b3c705602935fdfd17afdfcb6c6404
AldoCompagnoni <aldo.compagnoni@gmail.com>
2016-12-04 23:12:29 -0600

    First stab at vignette 2. More than 1/2 through. Missing sections on 'complicated searches', 'full_tbl', 'covariates' and whatever might end up revealing itself necessary.

---
+++ vignettes/vetting-popler.R
@@ -0,0 +1,21 @@
+## ---- warning = FALSE, message = FALSE-----------------------------------
+library(popler)
+browse(duration_years > 10)
+
+## ---- warning = FALSE, message = FALSE-----------------------------------
+dictionary("samplefreq")
+browse(samplefreq == "monthly")
+
+## ---- warning = FALSE, message = FALSE-----------------------------------
+dictionary("kingdom")
+browse(kingdom == "Plantae")
+browse(class == "Insect")
+
+## ---- warning = FALSE, message = FALSE-----------------------------------
+dictionary("community")
+browse(community == "no") # single-species studies
+
+## ---- warning = FALSE, message = FALSE-----------------------------------
+dictionary("community")
+browse(community == "no") # single-species studies
+
