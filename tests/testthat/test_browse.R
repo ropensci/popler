3781ab18f8662773eae5fe1ef73715ec88169916
AldoCompagnoni <aldo.compagnoni@gmail.com>
2016-12-07 12:12:57 -0600

    summary_popler() context.

--- tests/testthat/test_browse.R
+++ tests/testthat/test_browse.R
@@ -113,7 +113,7 @@ test_that("browse() function ", {
 })
 
 
-# browse function itself --------------------------------------------------
+# dictionary function --------------------------------------------------
 context("dictionary() function")
 
 test_that("dictionary() function ", {
@@ -131,5 +131,30 @@ test_that("dictionary() function ", {
   expect_equal( class(dictionary("species")[[1]]), "data.frame")
   expect_equal( class(dictionary("lterid")[[1]]), "character")
   
+})
+
+
+
+# summary_popler() function --------------------------------------------------
+context("summary_popler() function")
+
+test_that("summary_popler() function ", {
+  
+  
+  # n of variables produced (this neeed to work)
+  expect_equal(dim( summary_popler() ), c(1,1) )
+  expect_equal(ncol( summary_popler(group_vars = "lterid",
+                                    count_vars = "species") ), 
+               2 )
+  expect_equal(ncol( summary_popler(group_vars = "lterid",
+                                    count_vars = c("species","order")) ), 
+               3 )
+  
+  # column names produced
+  expect_equal(names( summary_popler() ), "title_count" )
+  expect_equal(names( summary_popler(count_vars = "lterid") ), "lterid_count" )
+  expect_equal(names( summary_popler(count_vars = "class") ), "class_count" )
+
+
 })
 
