bb336159fec798a9efce5a4b9a71d01826687471
AldoCompagnoni <aldo.compagnoni@gmail.com>
2017-01-18 20:31:15 -0600

    'structure/treatment' special vars, NSE in the input (new '...' argument to specify variables), provides description of 'structure/treatment' as if they were a single variables instead of three variables.

--- R/dictionary.R
+++ R/dictionary.R
@@ -1,20 +1,20 @@
 #' Dictionary of the popler metadata
 #'
 #' Provides information on the columns of metadata contained in the popler database, and the kind of data contained in those columns.
-#' @param vars A character string specifying one column of popler's main table for which dictionary information is needed
+#' @param ... A sequence of (unquoted) variables specifying one or more columns of popler's main table for which dictionary information is needed
 #' @param full_tbl Should the function return the standard columns, or the full main table?
 #' @export
 #' @examples
 #' # Column names
-#' column_names <- dictionary(vars = NULL, full_tbl = FALSE)
+#' column_names <- dictionary(full_tbl = FALSE)
 #' 
 #' # Dictionary information
-#' dictionary_lter <- dictionary(vars = "lterid", full_tbl = FALSE)
+#' dictionary_lter <- dictionary(lterid, full_tbl = FALSE)
 #' 
 #' # multiple columns
-#' dictionary_lter_lat <- dictionary(vars = c("lterid","lat_lter"), full_tbl = FALSE)
+#' dictionary_lter_lat <- dictionary(lterid,lat_lter, full_tbl = FALSE)
 
-dictionary <- function(vars = NULL, full_tbl = FALSE){
+dictionary <- function(..., full_tbl = FALSE){
   
   # Load main data table and convert factors to characters
   main_t        <- popler:::factor_to_character(popler:::main_popler)
@@ -23,6 +23,7 @@ dictionary <- function(vars = NULL, full_tbl = FALSE){
   names(main_t) <- tolower( names(main_t) )
   main_t        <- popler:::class_order_names(main_t)
   possible_arg  <- popler:::possibleargs
+  vars          <- popler:::vars_dict(...)
   
   # if no column specified, return ALL column names
   if( is.null(vars) ){
