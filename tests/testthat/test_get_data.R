af1dbf4756557c3e93470a1afe81c025645397a5
aldocompagnoni <ac79@rice.edu>
2017-03-17 14:21:14 -0500

    concatenate_queries unit tests

--- tests/testthat/test_get_data.R
+++ tests/testthat/test_get_data.R
@@ -44,6 +44,62 @@ test_that("inherit_search", {
 })
 
 
+
+
+# one comment 
+concatenate_queries(poa_d, poa_d)
+concatenate_queries(year == 2000, year == 2000)
+concatenate_queries(poa_d, poa_d, obj)
+concatenate_queries(poa_d, obj)
+
+# COMMENTS
+# concatenate_queries(poa_d, poa_d) should specifically say that you entered two browse objects.
+# concatenate_queries(poa_d, poa_d, obj) should say that you specified 3 entries (not refer to obj as source of error) 
+
+
+
+# all conditinos in which concatenate queries should work  
+test_that("concatenate_queries", {
+
+  # set up test variables
+  
+  # a browse object
+  poa_d <- browse(genus == "Poa")
+  # a non-popler object 
+  obj   <- c(1,2)
+  
+  # expected outputs --------------------------------------------------
+  
+  # expected output - a call
+  expect_equal( class( concatenate_queries(poa_d) ), "call")
+  expect_equal( class( concatenate_queries(browse(genus == "Poa")) ), "call")
+  expect_equal( class( concatenate_queries(poa_d, year == 2000) ), "call")
+  expect_equal( class( concatenate_queries(browse(genus == "Poa"), year == 2000) ), "call")
+  
+  # expected output - at least one "&" in call if two elements are provided
+  call_conc <- concatenate_queries(browse(genus == "Poa"), year == 2000) %>%
+                deparse()
+  expect_true( grepl("&",call_conc))
+  
+  #expected errors ------------------------------------------------------
+  
+  #1. More than 2 arguments
+  expect_error( concatenate_queries(poa_d, poa_d, poa_d) )
+  expect_error( concatenate_queries(obj, obj, obj) )
+  expect_error( concatenate_queries(poa_d, poa_d, obj, obj) )
+  expect_error( concatenate_queries(year == 2000, year == 2000, year == 2000) )
+  #2. two logical statements
+  expect_error( concatenate_queries(year == 2000, year == 2000) )
+  #3. two popler objects
+  expect_error( concatenate_queries(poa_d, poa_d) )
+  #4. any object that is not a popler object
+  expect_error( concatenate_queries(poa_d, poa_d) )
+  
+  rm( list = c("poa_d", "obj") )
+  
+})
+
+
 # Does inherit_variables() accurately identify the variables mentioned in MULTIPLE logical statement?   
 test_that("inherit_variables", {
   
