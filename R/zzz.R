19949cccbc4f00a1be40009040d5e4724ffd967b
Brad Ochocki <brad.ochocki@gmail.com>
2017-03-25 21:20:15 -0500

    summary_table_check() runs on library(popler)
    added zzz.R and .onLoad(). 
    
    now when the popler library is loaded, summary_table_check() tests whether the summary table exists and/or needs to be updated. (referenced here: https://github.com/AldoCompagnoni/popler/issues/27#issuecomment-289253288)
    
    
    
    changed `summary_table` references in `browse()` to `popler::summary_table`

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+.onLoad <- function(libname = find.package("popler"), pkgname = "popler") {
+  
+  # just a goofy test message
+  message("Welcome to popler, y'all!")
+  
+  # check if the summary table exists or needs to be updated
+  summary_table_check()
+  
+  # load the summary table into the namespace
+  devtools::load_data()
+}
