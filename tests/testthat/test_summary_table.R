daae71dffcd997a99f2b1059ffbaa31f98f70031
AldoCompagnoni <aldo.compagnoni@gmail.com>
2017-04-03 16:22:54 -0500

    Changed just two spaces

--- tests/testthat/test_summary_table.R
+++ tests/testthat/test_summary_table.R
@@ -3,7 +3,6 @@ context("summary_popler() function")
 
 test_that("summary_popler() function ", {
   
-  
   # n of variables produced (this neeed to work)
   expect_equal(dim( summary_popler() ), c(1,1) )
   expect_equal(ncol( summary_popler(group_vars = "lterid",
@@ -17,7 +16,6 @@ test_that("summary_popler() function ", {
   expect_equal(names( summary_popler() ), "title_count" )
   expect_equal(names( summary_popler(count_vars = "lterid") ), "lterid_count" )
   expect_equal(names( summary_popler(count_vars = "class") ), "class_count" )
-  
-  
+
 })
 
