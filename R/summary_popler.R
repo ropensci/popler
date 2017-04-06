1b6154ddfc36be9f2a04d501facee324dacb3f0d
AldoCompagnoni <aldo.compagnoni@gmail.com>
2017-04-06 15:42:42 -0500

    Include utility functions
    Utility functions are `tallies` and `multiple_columns`

--- R/summary_popler.R
+++ R/summary_popler.R
@@ -29,3 +29,62 @@ summary_popler <- function(group_vars = NULL, count_vars = "title", trim = TRUE)
   return(out) # return output
 
 }
+
+# Calculate tallies
+tallies=function(browsed_data,tally_columns,group_factors,trim){
+  
+  df_list=list()
+  for(i in 1:length(tally_columns)){
+    
+    #does 'tally_columns' refers to multiple columns
+    multi_tally=multiple_columns(tally_columns[i])
+    
+    #columns referring to both group_factors and tallies
+    group_tally_cols=c(group_factors,multi_tally)
+    
+    #Data of interest for the tally
+    tally_data=select(browsed_data,one_of(group_tally_cols))
+    
+    #lower case of grouping factors (should get rid of doubles such as "yes" "Yes")
+    tally_data=as.data.frame(tally_data)
+    for(co in 1:ncol(tally_data)) { tally_data[,co]=tolower(tally_data[,co])}
+    tally_data=as.tbl(tally_data)
+    
+    #only unique values
+    tally_data=distinct(tally_data)
+    
+    #store tallies
+    tally_name=paste0(tally_columns[i],"_count") #name of column containing tallies
+    #grouping factor present
+    if(!is.null(group_factors)){ 
+      df_list[[i]] <- tally_data %>%
+        group_by_(.dots=group_factors) %>%
+        summarise_(.dots=setNames(list(~n()), tally_name))
+      #No grouping factor
+    } else {
+      df_list[[i]] <- tally_data %>% summarise_(.dots=setNames(list(~n()),tally_name))
+    }
+    
+  }
+  out=trim_display(Reduce(function(...) merge(...),df_list),trim)
+  return(out)
+  
+}
+
+
+# function converts entries to multiple columns - if need be
+multiple_columns=function(x) {
+  
+  taxonomy            <-  c("kingdom","phylum","clss","ordr","family","genus","species")
+  species             <-  c("genus","species")
+  spatialLevels       <-  c("sp_rep1_ext","sp_rep2_ext","sp_rep3_ext","sp_rep4_ext")
+  wrapperNames        <-  c("taxonomy","species","spatialLevels") #list of multiple columns entries (the above threee)
+  
+  if( any(x %in% wrapperNames) ){
+    oldIds   <- which(x %in% wrapperNames)
+    newIds   <- which(wrapperNames %in% x)
+    eval(parse(n=1,text=paste0("newArg=",wrapperNames[newIds])))
+    columnNames=c(x[-oldIds],newArg)
+    return(columnNames)
+  } else(return(x))
+}
