510e2c66e585a92a8fcf2bf0698cf7cc423a6d8f
Brad Ochocki <brad.ochocki@gmail.com>
2017-04-11 15:07:15 -0500

    debugging instal

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
   message("Welcome to popler, y'all!")
   
   # check if the summary table exists or needs to be updated
-  summary_table_check()
+  #summary_table_check()
   
   # load the summary table into the namespace
   devtools::load_data()
