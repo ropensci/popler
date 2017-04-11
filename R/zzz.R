1526e244e8d64f3e2815362d407f4651b5b7b35a
Brad Ochocki <brad.ochocki@gmail.com>
2017-04-11 16:04:37 -0500

    more testing

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,16 @@
 .onLoad <- function(libname = find.package("popler"), pkgname = "popler") {
   
-  # just a goofy test message
-  message("Welcome to popler, y'all!")
-  
   # check if the summary table exists or needs to be updated
   summary_table_check()
   
+  message("summary table done")
+  
+  message(system.file("extdata", "summary_table.rda", package = "popler"))
+  
   # load the summary table into the namespace
-  load(system.file("/extdata", "summary_table.rda", package = "popler"), envir=parent.env(environment()), verbose=FALSE)
+  load(system.file("extdata", "summary_table.rda", package = "popler"), envir=parent.env(environment()), verbose=FALSE)
+  
+  # just a goofy test message
+  message("Welcome to popler, y'all!")
+  
 }
