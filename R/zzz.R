37886cfc8cffb21e0cd3c03956b3486070828108
Brad Ochocki <brad.ochocki@gmail.com>
2017-04-11 15:01:40 -0500

    uncomment lines for install check

--- R/zzz.R
+++ R/zzz.R
@@ -4,8 +4,8 @@
   message("Welcome to popler, y'all!")
   
   # check if the summary table exists or needs to be updated
-  #summary_table_check()
+  summary_table_check()
   
   # load the summary table into the namespace
-  #devtools::load_data()
+  devtools::load_data()
 }
