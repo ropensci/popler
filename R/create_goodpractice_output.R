2ea4260979c5edb5d907a5385a7c8c94569ce360
levisc8 <levisc8@gmail.com>
2017-12-13 18:31:20 +0100

    remove unnecessary scripts

--- R/create_goodpractice_output.R
+++
@@ -1,6 +0,0 @@
-# Runs goodpractice without lintrs. currently, all linters fail for an unknown reason
-# on report_metadata.R. We need to investigate this, but unsure of how to resolve at the moment
-# also adding to .Rbuildignore
-
-GPOutput <- goodpractice::gp(checks = goodpractice::all_checks()[!grepl('lintr_', 
-                                                                        goodpractice::all_checks())])
